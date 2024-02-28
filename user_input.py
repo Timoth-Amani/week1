8b95866a47afe0b00178696f9409be833b9a9baa
Timoth-Amani <timothamani1@gmail.com>
2024-02-28 20:50:52 +0300

    first commit

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input('What is your name?')
+age = int(input('What is your name?'))
+location = input('What is your name?')
+
+print(f"Hallo {name}, you are {age} years old and live in {location}.")
